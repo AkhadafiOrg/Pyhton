1f4ffd7488c6e886707f54a399634b24da9b2c4f
Akhadafi <khadafi.dev@gmail.com>
2022-07-13 20:13:27 +0700

    Tipe Data

---
+++ Python/02 Tipe Data/Main.py
@@ -0,0 +1,58 @@
+# Tipe Data Bawaan
+# Python memiliki tipe data berikut bawaan secara default, dalam kategori ini:
+"""
+Text Type     :	str
+Numeric Types :	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type  :	dict
+Set Types     :	set, frozenset
+Boolean Type  :	bool
+Binary Types  :	bytes, bytearray, memoryview
+None Type     :	NoneType
+"""
+
+# Mendapatkan Tipe Data
+# Anda bisa mendapatkan tipe data objek apa pun dengan menggunakan fungsi type() :
+x = 5
+print(type(x))
+
+# Mengatur Tipe Data
+# Di Python, tipe data diatur saat Anda menetapkan nilai ke variabel:
+"""
+Example	                                      Data Type
+x = "Hello World"	                            str	
+x = 20	                                      int	
+x = 20.5                                      float	
+x = 1j	                                      complex	
+x = ["apple", "banana", "cherry"]	            list	
+x = ("apple", "banana", "cherry")	            tuple	
+x = range(6)	                                range	
+x = {"name" : "John", "age" : 36}	            dict	
+x = {"apple", "banana", "cherry"}	            set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+x = True	                                    bool	
+x = b"Hello"	                                bytes	
+x = bytearray(5)	                            bytearray	
+x = memoryview(bytes(5))	                    memoryview	
+x = None	                                    NoneType
+"""
+
+# Mengatur Jenis Data Tertentu
+# Jika Anda ingin menentukan tipe data, Anda dapat menggunakan fungsi konstruktor berikut:
+"""
+Example	                                      Data Type
+x = str("Hello World")	                      str	
+x = int(20)	                                  int	
+x = float(20.5)	                              float	
+x = complex(1j)	                              complex	
+x = list(("apple", "banana", "cherry"))	      list	
+x = tuple(("apple", "banana", "cherry"))	    tuple	
+x = range(6)	                                range	
+x = dict(name="John", age=36)	                dict	
+x = set(("apple", "banana", "cherry"))	      set	
+x = frozenset(("apple", "banana", "cherry"))	frozenset	
+x = bool(5)	                                  bool	
+x = bytes(5)	                                bytes	
+x = bytearray(5)	                            bytearray	
+x = memoryview(bytes(5))	                    memoryview
+"""
